1ad53f5ee1d5d89ac5c9849e599dcdf44b9ebb23
John Melody Me <johnmelodyme@yandex.com>
2022-12-30 19:34:45 +0800

    added basic pages, front page, added assets and designs

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+const ViewResume = () => {
+  window.open("assets/pdf/johnmelodyme::resume.pdf", "_blank");
+};
+
+const VisitPawlynPet = () => {
+  window.location.href = "http://pawlynpet.softr.app";
+};
+
+const OpenBybitAffliate = () => {
+  window.location.href = "https://www.bybit.com/en-US/invite/?ref=53XZLX";
+};
+
+const RedirectToPortfolio = () => {
+  window.location.href = "views/portfolio.html";
+};
